fc7736ff99f1036d81a183296debcd847c6e9851
miguel <miguel.baron01@correo.usa.edu.co>
2023-05-04 15:09:36 -0500

    perfil html y verificacion de que los datos se guarden bien en la base de datos

--- src/main/resources/static/js/myScript.js
+++ src/main/resources/static/js/myScript.js
@@ -20,18 +20,24 @@
 		 success: function(rta) {
 			 $("#username").val("");
 			 $("#password").val("");
-			 Cookies.set('token', rta.token);
-			 window.location.replace();
+			 Cookies.get('token');
+			 window.location.replace("inicio.html");
 		 },
 		 error: function(xhr, status) {
-			 alert('Disculpe, existió un problema');
+			 alert('Usuario no existente');
 		 },
 		 complete: function(xhr, status) {
-			 alert('Petición realizada');
+			 //alert('Petición realizada');
 		 }
 
 	 });
  }
+
+ function logOut(){
+	Cookies.get('token');
+	window.location.replace("index.html");
+ }
+
 
  function registerData(){
 
@@ -63,6 +69,81 @@
 			 $("#email").val("");
 			 $("#password").val("");
 			 window.location.replace("pagina_inicio_1.html")
+			 Cookies.set('token', rta.token);
+		 },
+		 error: function(xhr, status) {
+			 alert('Disculpe, existió un problema');
+		 },
+		 complete: function(xhr, status) {
+			 //alert('Petición realizada');
+		 }
+
+	 });
+ }
+
+ function sendQuestion(){
+
+	 let titulo = $("#titulo").val();
+	 let contenido = $("#contenido").val();
+
+	 let question = {
+		 titulo: titulo,
+		 contenido: contenido
+	 }
+
+	 $.ajax({
+
+		url:"/api/v1/forum/create",
+		type:"POST",
+		contentType:"application/json",
+		dataType:"json",
+
+		data:JSON.stringify(question),
+
+		success: function(rta) {
+			console.log(rta);
+			$("#titulo").val("");
+			$("#contenido").val("");
+		},
+		error: function(xhr, status) {
+			alert('Disculpe, existió un problema');
+		},
+		complete: function(xhr, status) {
+			//alert('Petición realizada');
+		}
+
+	});
+ }
+
+ function CheckingA() {
+	 // seleccionar todas las casillas de verificación en la página
+	 const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+	 let areas = [];
+
+	 // iterar sobre las casillas de verificación y encontrar las que están marcadas
+	 for (let i = 0; i < checkboxes.length; i++) {
+		 if (checkboxes[i].checked) {
+			 areas.push(checkboxes[i].value);
+		 }
+	 }
+
+	 $.ajax({
+
+		 url:"/api/v1/auth/register",
+		 type:"POST",
+		 contentType:"application/json",
+		 dataType:"json",
+
+		 data:JSON.stringify(data),
+
+		 success: function(rta) {
+			 console.log(rta);
+			 $("#name").val("");
+			 $("#lastname").val("");
+			 $("#email").val("");
+			 $("#password").val("");
+			 window.location.replace("pagina_inicio_2.html")
 
 		 },
 		 error: function(xhr, status) {
@@ -75,20 +156,60 @@
 	 });
  }
 
- function Checking() {
+ function CheckingC() {
 	 // seleccionar todas las casillas de verificación en la página
 	 const checkboxes = document.querySelectorAll('input[type="checkbox"]');
 
-	 let values = [];
+	 let carreras = [];
 
 	 // iterar sobre las casillas de verificación y encontrar las que están marcadas
 	 for (let i = 0; i < checkboxes.length; i++) {
 		 if (checkboxes[i].checked) {
-			 values.push(checkboxes[i].value);
+			 carreras.push(checkboxes[i].value);
 		 }
 	 }
-	 console.log(values);
+
+	 $.ajax({
+
+		 url:"/api/v1/auth/register",
+		 type:"POST",
+		 contentType:"application/json",
+		 dataType:"json",
+
+		 data:JSON.stringify(data),
+
+		 success: function(rta) {
+			 console.log(rta);
+			 $("#name").val("");
+			 $("#lastname").val("");
+			 $("#email").val("");
+			 $("#password").val("");
+			 window.location.replace("pagina_inicio_3.html")
+
+		 },
+		 error: function(xhr, status) {
+			 alert('Disculpe, existió un problema');
+		 },
+		 complete: function(xhr, status) {
+			 //alert('Petición realizada');
+		 }
+
+	 });
  }
- 
- 
+
+ function CheckingM() {
+	 // seleccionar todas las casillas de verificación en la página
+	 const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+	 let materias = [];
+
+	 // iterar sobre las casillas de verificación y encontrar las que están marcadas
+	 for (let i = 0; i < checkboxes.length; i++) {
+		 if (checkboxes[i].checked) {
+			 materias.push(checkboxes[i].value);
+		 }
+	 }
+	 window.location.replace("inicio.html")
+
+ }
  
